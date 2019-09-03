e9f946062337e8c1cbe28899b82c9a7be34ca36d
Matthew McRaven <mkm302@goergetown.edu>
2019-09-02 21:03:21 -0400

    Bug fixes for test of assembly program samples.
    Add additional descriptions to each line of the program assembly tests.

--- pep10unit/MacroAssembler/tst_assembleprograms.cpp
+++ pep10unit/MacroAssembler/tst_assembleprograms.cpp
@@ -14,14 +14,17 @@ void AssemblePrograms::initTestCase()
 {
     osText = Pep::resToString(":/help-asm/figures/pep10os.pep", false);
     QVERIFY2(!osText.isEmpty(), "Default operating system was empty.");
+
     QSharedPointer<AsmProgram> prog;
     auto elist = QList<QPair<int, QString>>();
     MacroAssemblerDriver assembler(registry);
     auto asmResult = assembler.assembleOperatingSystem(osText);
+
     QVERIFY2(asmResult.success, "Assembly of operating system did not succede");
     QVERIFY2(!asmResult.program.isNull(), "Operating system program does not exist");
     QVERIFY2(asmResult.program->getProgram().length() != 0,
              "Operating system program contains no code.");
+
     operatingSystem = asmResult.program;
 }
 
@@ -31,9 +34,9 @@ void AssemblePrograms::assemblePrograms_data()
     QDirIterator it = QDirIterator(":/help-asm/figures/", QStringList()<<"fig*.pep");
     while(it.hasNext()) {
         auto file = it.next();
-        qDebug() << file;
         QFileInfo info(file);
-        QTest::newRow(info.fileName().toStdString().c_str())
+        QString str = "Assemble file "+info.fileName();
+        QTest::newRow(str.toStdString().c_str())
                 << Pep::resToString(file, false);
     }
 }
@@ -41,11 +44,14 @@ void AssemblePrograms::assemblePrograms_data()
 void AssemblePrograms::assemblePrograms()
 {
     QFETCH(QString, ProgramText);
+
     MacroAssemblerDriver assembler(registry);
     auto osSymTable = operatingSystem->getSymbolTable();
     auto asmResult = assembler.assembleUserProgram(ProgramText, operatingSystem->getSymbolTable());
-    QVERIFY2(asmResult.success, "Assembly of sample system did not succede");
-    QVERIFY2(!asmResult.program.isNull(), "Sample program does not exist");
+
+    QVERIFY2(asmResult.success, "Assembly of sample program did not succede.");
+    QVERIFY2(asmResult.errors.errors.empty(), "Sample program had warnings or errors.");
+    QVERIFY2(!asmResult.program.isNull(), "Sample program does not exist.");
     QVERIFY2(asmResult.program->getProgram().length() != 0,
              "Sample program contains no code.");
 
